36fd79de05bf365f2e7a224dd75e812b94fd6c55
marwan-mohamed-s <marawan34567890@gmail.com>
2025-03-02 11:51:50 -0800

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 
 const FeedbackSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
+  userId: { type: String, ref: "User", required: true },
+  courseId: { type: String, ref: "Course", required: true },
   comment: { type: String, required: true },
   rating: { type: Number, required: true, min: 1, max: 10 },
 }, { timestamps: true });
 const Feedback = mongoose.model("Feedback", FeedbackSchema);
 
-module.exports = {Feedback}
+module.exports = { Feedback }
