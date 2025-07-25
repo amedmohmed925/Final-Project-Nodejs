bb203e6436becaf233c0edb22067445c0158ade2
Ahmed Mohamed <118309402+amedmohmed925@users.noreply.github.com>
2025-07-25 07:11:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -170,4 +170,4 @@ io.on("connection", (socket) => {
 
 server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
-module.exports = { app, io };
+module.exports = app;
